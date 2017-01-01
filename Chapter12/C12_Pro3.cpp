49cc801dd3c4f2ba8361ef319a629465b0ba5a86
root <root@localhost>
2017-01-01 14:03:19 +0800

    完成

--- Chapter12/C12_Pro3.cpp
+++ Chapter12/C12_Pro3.cpp
@@ -3,12 +3,14 @@
 
 int main(void){
 	char c[M];
+	int i = 0;
 	printf("Enter a message:");
-	while ((*(c++) = getchar()) != '\n'
-		&& (c - &c[0]) != M);
+	while ((*(c + i++) = getchar()) != '\n'
+		&& i != M);
+	i--;
 //	printf("%d",p-c);
 	printf("Reversal is: ");
-	while (c-- !=&c[0])
-		printf("%c",*(c-1));
+	while (i-- !=0)
+		printf("%c",*(c+i));
 	return 0;
 }
