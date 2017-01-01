dec3aa3da7ff10092e5f576020b31aa7dfd28fc7
root <root@localhost>
2017-01-01 22:33:21 +0800

    未完成

--- Chapter15/E.cpp
+++ Chapter15/E.cpp
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
-#define GOOD_MAX 100
+#define MAX 100
+#define FILE_NAME "good_info.txt"
 
 int e = 0;
 /*			GoodInfo* pGoodInfo = (GoodInfo*)malloc(sizeof(GoodInfo));
@@ -15,95 +17,219 @@ int e = 0;
 
 
 typedef struct {
-  int id;
+  char id[30];
   char name[20];
-  float price;
-  float discount;
+  char price[10];
+  char discount[10];
   int amount;
-} Goods;
+  int remain;
+} Goods ;
 
-Goods all_g[GOOD_MAX];
+Goods AGo[MAX] ;
+	
 
-void edit_data(Goods g);
-void delete_data(Goods g);
-void inquiry_data(Goods g[]);
-void enter_data(Goods g[]);
-
-all_g[0]{
-	id = 000;
-	name[] = "kfdykme";
-	price = 2.2;
-	discount = 0.7;
-	amount = 1;
-	}
-
-all_g[1] = {
-	id = 001;
-	name[] = "可乐";
-	price = 2.5;
-	discount = 1;
-	amount = 100;
-	}
+void delete_D(Goods g[]);
+void edit(Goods g[]);
+void enter(Goods g[]);
+void goods_info_ini();
+void inquiry(Goods g[]);
 
 
 
 int main(void) {
-  int order_number;
+	int order_number;
+	goods_info_ini();
+	while (order_number != 5) {
+    	printf("\n-----------------------------------------------------------\n"
+    	       "\n"
+    	       "输入数字来选择功能：\n"
+     	      "\t 1. 商品信息修改\n"
+     	      "\t 2. 商品信息删除\n"
+     	      "\t 3. 商品信息查询\n"
+     	      "\t 4. 商品信息录入\n"
+     	      "\t 5. 推出系统并保存\n"
+     	      "\t 6. 显示所有商品信息"
+     	      "\n"
+     	      "\n-----------------------------------------------------------\n");
 
+	    scanf("%d", &order_number);
 
-
-  while (order_number != 5) {
-    printf("输入数字来选择功能：\n"
-           "\t 1. 商品信息修改\n"
-           "\t 2. 商品信息删除\n"
-           "\t 3. 商品信息查询\n"
-           "\t 4. 商品信息录入\n" "\t 5. 推出系统并保存\n" "");
-
-    scanf("%d", &order_number);
-
-    switch (order_number) {
-    case 1:
-      // edit_data();
-      break;
-    case 2:
-      // delete_data();
-      break;
-    case 3:
-       inquiry_data(all_g);
-      break;
-    case 4:
-      enter_data(all_g);
-
-      break;
-    default:
-  	printf("无效操作\n\n");
-      break;
+	    switch (order_number) {
+		    case 1:
+		      edit(AGo);
+ 		   break;
+  		  case 2:
+   		  delete_D(AGo);
+    		  break;
+    		case 3:
+   			inquiry(AGo);
+     		     break;
+    		case 4:
+     		 enter(AGo);
+		      break;
+		case 6:
+			for ( int i = 0 ; i < e;i++)
+					printf("\nGoods' info of %s :\n"
+			 		      "\tid: %s\n"
+			   		    "\tname: %s\n"
+			  		     "\tprice: %sf\n"
+			    		   "\tdiscount: %s\n"
+			     		  "\tamount: %d\n"
+			     		  "\tremain: %d\n",
+			       AGo[i].name,
+			       AGo[i].id,
+			       AGo[i].name,
+			       AGo[i].price,
+			       AGo[i].discount,
+			       AGo[i].amount,
+			       AGo[i].remain);
+			break;
+ 		   default:
+  			printf("无效操作\n\n");
+   		   break;
     }
   }
 
   return 0;
 }
 
-void enter_data(Goods g[]) {
+	
+	
+void delete_D(Goods g[]){
+	printf("\nThere are the Goods:\n");	
+	for (int i = 0; i <e;i++)
+		printf("Id\t%s\tname\t%s\n",g[i].id,g[i].name);
+	printf("\nEnter Goods' id to delete a Goods: ");
+	char did[30];
+	bool isDeleteStart =false;
+	scanf("%s",did);
+	for (int i = 0; i < e;i++){
+		if (!strcmp(g[i].id,did))
+			isDeleteStart = true;
+		if (isDeleteStart)
+			g[i] = g[i+1];
+		if (isDeleteStart && i  == e - 1){
+			e--;
+		}
+	}
+	
+	for (int i = 0; i <e;i++)
+		printf("Id\t%s\tname\t%s\n",g[i].id,g[i].name);
+		
+	printf( "There are the Goods now: \n");
+	if (isDeleteStart)
+		printf("Delete successfully.\n");
+	else 
+		printf("Delete  unsuccessfully.\n");
+	
+}
+void edit(Goods g[]){
+		printf("\nThere are the Goods:\n");	
+	for (int i = 0; i <e;i++)
+		printf("Id\t%s\tname\t%s\n",g[i].id,g[i].name);
+
+	printf("\n"
+	       "Enter the Goods' id to edit it: ");
+	char id[30];
+	scanf("%s",id);
+	
+	Goods* tg = (Goods*)malloc(sizeof(Goods));
+			
+	for (int i = 0; i < e; i++)
+		if (!strcmp(g[i].id,id)){
+			*tg = g[i];
+			printf("id: %s to : ",tg->id);
+			scanf("%s",tg->id);
+			for (int j = 0 ; j < e; j++){
+			}
+			printf("name: %s to : ",tg->name);
+			scanf("%s",tg->name);
+			printf("price: %s to : ",tg->price);
+			scanf("%s",tg->price);
+			printf("discount: %s to : ",tg->discount);
+			scanf("%s",tg->discount);
+			printf("amount: %d to : ",tg->amount);
+			scanf("%d",&tg->amount);
+			printf("remain: %d to : ",tg->remain);
+			scanf("%d",&tg->remain);
+			g[i] = *tg;
+			break;
+		}
+	
+}
+			
+			
+
+
+void enter(Goods g[]) {
 
     printf("Enter goods' id: ");
-    scanf("%d",&g[e].id);
+    scanf("%s",&g[e].id);
     printf("Enter goods' name: ");
     scanf("%s", g[e].name);
     printf("Enter goods' price($): ");
-    scanf("%f", &g[e].price);
+    scanf("%s", &g[e].price);
     printf("Enter goods' discount: ");
-	scanf("%f",&g[e].discount);
+	scanf("%s",&g[e].discount);
 	printf("Enter goods amount: ");
-	scanf("%d",&g[e].amount);         
+	scanf("%d",&g[e].amount);  
+	printf("Enter goods remain: ");
+	scanf("%d",&g[e].remain);                
   e++;
+}
+	
+void goods_info_ini(){
+	FILE* fp;
+	fp = fopen(FILE_NAME,"r");
+
+	Goods* g = (Goods*)malloc(sizeof(Goods));
+
+	//g = AGo;
+	if( fp == NULL){
+		printf("Can't open %s",FILE_NAME);
+		exit(0);	
+	}
+	char s[100];
+	int i;
+	printf("Had open %s\n", FILE_NAME);
+	
+	while(1){
+		fscanf(fp, "id:%s\n",s);
+		strcpy(g->id,s);
+		
+		if ( !strcmp(s,"end") )break;
+		printf("  id:%s\n",g->id);
+		
+		fscanf(fp, "name:%s\n",s);
+		strcpy(g->name,s);
+		printf("  name:%s\n",g->name);
+		
+		fscanf(fp, "price:%s\n",s);
+		strcpy(g->price,s);
+		printf("\tprice:%s\n",g->price);
+		
+		fscanf(fp, "discount:%s\n",s);
+		strcpy(g->discount,s);
+		printf("\tdiscount:%s\n",g->discount);
+		
+		fscanf(fp, "amount:%d\n",&i);
+		g->amount = i;
+		printf("\tamount:%d\n",g->amount);
+		fscanf(fp, "remain:%d\n",&i);
+		g->remain = i;
+		printf("\tremain:%d\n",g->remain);
+		AGo[e++] = *g;
+	} 	
+	
+	fclose(fp); 	
 }
 
 
-void inquiry_data(Goods g[]){
+
+void inquiry(Goods g[]){
 	
 	char name[20];
-	int id;
+	char  id[30];
 	int choice;
 	
 	printf("You can inquiry goods' data by \n"
@@ -116,22 +242,24 @@ void inquiry_data(Goods g[]){
 		case 1:
 			printf("Please enter the id: ");
 			
-			scanf("%d",&id);
+			scanf("%s",id);
 	
-			for( int i = 0 ; i < e;i++){
-				if (g[i].id == id){
+			for( int i = 0 ; i < MAX;i++){
+				if (!strcmp(g[i].id,id)){
 					printf("Goods' info of %d :\n"
-			 		      "\tid: %d\n"
+			 		      "\tid: %s\n"
 			   		    "\tname: %s\n"
-			  		     "\tprice: %0.2f\n"
-			    		   "\tdiscount: %0.2f\n"
-			     		  "\tamount: %d\n",
+			  		     "\tprice: %s\n"
+			    		   "\tdiscount: %s\n"
+			     		  "\tamount: %d\n"
+			     		  "\tremain: %d\n",
 			       g[i].id,
 			       g[i].id,
 			       g[i].name,
 			       g[i].price,
 			       g[i].discount,
-			       g[i].amount);
+			       g[i].amount,
+			       g[i].remain);
 			       return;
 				}
 			}
@@ -142,21 +270,23 @@ void inquiry_data(Goods g[]){
 			
 			scanf("%s",name);
 	
-			for( int i = 0 ; i < e;i++){
+			for( int i = 0 ; i < MAX;i++){
 				if (!strcmp( g[i].name,name)){
 					printf("Goods' info of %s :\n"
-			 		      "\tid: %d\n"
+			 		      "\tid: %s\n"
 			   		    "\tname: %s\n"
-			  		     "\tprice: %0.2f\n"
-			    		   "\tdiscount: %0.2f\n"
-			     		  "\tamount: %d\n",
+			  		     "\tprice: %s\n"
+			    		   "\tdiscount: %s\n"
+			     		  "\tamount: %d\n"
+			     		  "\tremain: %d\n",
 			       g[i].name,
 			       g[i].id,
 			       g[i].name,
 			       g[i].price,
 			       g[i].discount,
-			       g[i].amount);
-				   return;
+			       g[i].amount,
+			       g[i].remain);
+				 return;
 				}
 			}
 		default:
@@ -165,5 +295,3 @@ void inquiry_data(Goods g[]){
 	}
 	
 }
-	
-
