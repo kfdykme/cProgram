3f4867532b313351999545a864436e42d423001b
root <root@localhost>
2017-01-19 15:57:38 +0800

    完成

--- C13_Pro2(b).c
+++ C13_Pro2(b).c
@@ -9,46 +9,47 @@ int read_line(char str[], int n);
 int main(void){
 	
 	char reminders[MAX_REMIND][MSG_LEN+3];
-	char day_str[3], msg_str[MSG_LEN+1];
-	int day, i, j, num_remind = 0, hour, min;
-
+	char day_str[9], msg_str[MSG_LEN+1];
+	int day, i, j, num_remind = 0;
+	int hour, min;
+	
 	for (;;){
 		if (num_remind == MAX_REMIND){
 			printf("--No space left --\n");
 			break;
 		}
 		
-	printf("Enter day , 24-hour system time and reminder: \n");
-	scanf("%2d",&day);
-	
+	printf("Enter day and reminder: ");
+	scanf("%2d", &day);
 	if (day == 0)
 		break;
-	
-	scanf(" %2d %2d",  &hour, &min);
 		
-	sprintf(day_str, "%2d %2d %2d",day,hour, min);
+	scanf("%2d:%2d",&hour,&min);
+/* 要注意 day_str 的长度*/
+	sprintf(day_str,"%2d %2d:%2d",day,hour,min);
 	
 	read_line(msg_str,MSG_LEN);
-	
-	for ( i = 0 ; i < num_remind ; i++)
-		if (strcmp(day_str, reminders[i]) < 0)
-			break;	
-	printf("i = %d\n",i);		
-	
-	strcpy(reminders[j], reminders[i]);
-	
-	printf("\nreminders[%d] = %s \nreminders[%d] = %s\n reminders[%d] = %s\n",i+1 , reminders[i+1], i, reminders[i], i-1, reminders[i-1]);
-	
-	strcpy(reminders[i], day_str);
-	strcat(reminders[i], msg_str);
 	
 	if ( day > 31 || day < 0){
 		strcpy(reminders[num_remind++],"____Eor.");
 		continue;
 	}
 	
-	num_remind++;
+	for ( i = 0 ; i < num_remind ; i++)
+		if (strcmp(day_str, reminders[i]) < 0)
+			break;
 	
+	
+	
+	for (j = num_remind; j > i; j--)
+		strcpy(reminders[j], reminders[j-1]);
+		
+	
+	
+	strcpy(reminders[i], day_str);
+	strcat(reminders[i], msg_str);
+	
+	num_remind++;
 	}
 	
 	printf("\Day Reminder\n");
