9814756924c736ad27a86acb9da81b4fb0690a04
root <root@localhost>
2017-01-19 21:19:32 +0800

    ok

--- C13_Pro5.c
+++ C13_Pro5.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
 #include <string.h>
 
+int main(int argc, char *argv[]){
+	int sum = 0;
+	if (!strcmp(argv[1],"sum"))
+		for (int i = 2; i < argc ; i++)
+			sum += atoi(argv[i]);
+
+	printf("Total: %d",sum);
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+/*
+#include <stdio.h>
+#include <string.h>
+
 
 int main(void){
 	int i = 0,num[100], sum = 0;
@@ -17,3 +40,4 @@ int main(void){
 	printf("Total: %d ",sum);
 	return 0;
 }
+*/
